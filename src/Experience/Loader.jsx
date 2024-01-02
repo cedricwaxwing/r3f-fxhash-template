ade308cb6e29dd2a7efee64c0588ec984c41eff0
Colin Spence <udappia@gmail.com>
2024-01-01 23:37:16 -0700

    fake loading

--- src/Experience/Loader.jsx
+++ src/Experience/Loader.jsx
@@ -1,20 +1,28 @@
 import { Html, useProgress } from "@react-three/drei";
 import { useFeatures } from "../common/FeaturesProvider";
+import { useState } from "react";
 
 const Loader = () => {
   const { theme } = useFeatures();
   const { progress } = useProgress();
+  const [loaded, setLoaded] = useState(false);
 
+  if (!loaded && progress > 90) {
+    setLoaded(true);
+  }
   return (
     <Html center>
       <div className="loader">
         <div className="counter" style={{ color: theme.primary }}>
-          {progress.toFixed(0)}%
+          {loaded ? 100 : progress.toFixed(0)}%
         </div>
         <div className="progressbar">
           <span
             className="progress"
-            style={{ background: theme.primary, width: `${progress}%` }}
+            style={{
+              background: theme.primary,
+              width: `${loaded ? 100 : progress}%`,
+            }}
           ></span>
         </div>
       </div>
