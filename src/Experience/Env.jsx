92b1e2d8ad40c45fbea375281c8fd02ebd4f3bbe
Colin Spence <udappia@gmail.com>
2024-01-15 22:24:47 -0700

    updates Env

--- src/Experience/Env.jsx
+++ src/Experience/Env.jsx
@@ -1,28 +1,60 @@
-import { Environment, Sphere } from "@react-three/drei";
+import {
+  Environment,
+  Sphere,
+  ContactShadows,
+  SoftShadows,
+} from "@react-three/drei";
 import { useFeatures } from "../common/FeaturesProvider";
-import { blendColors } from "../common/utils";
+import { blendColors, mapValue, random_num } from "../common/utils";
 import { useLoader } from "@react-three/fiber";
 import environmentFile from "../assets/hdri/kloppenheim_06_puresky_2k.hdr";
 import { BackSide } from "three";
 import { useControls } from "leva";
 import { RGBELoader } from "three-stdlib";
+import { useMemo } from "react";
 
-const Env = () => {
-  const { theme, lighting } = useFeatures();
+const Env = ({ groundY }) => {
+  const { theme, lighting, envRotation, envIntensity } = useFeatures();
   const environmentTexture = useLoader(RGBELoader, environmentFile);
 
-  const { y, intensity } = useControls({
-    y: { value: 2.76, min: -Math.PI, max: Math.PI },
-    intensity: { value: 0.9, min: 0.5, max: 1 },
-  });
+  const position = useMemo(() => {
+    const radius = 10000;
+    const sunOffset = -0.1;
+
+    // Calculate x and z
+    const x = radius * Math.cos(-envRotation + sunOffset);
+    const z = radius * Math.sin(-envRotation + sunOffset);
+
+    const minY = 8;
+    const maxY = 7000;
+
+    const minRotation = -0.1;
+    const maxRotation = -2.35;
+
+    let normalizedRotation =
+      (envRotation - minRotation) / (maxRotation - minRotation);
+
+    normalizedRotation = Math.max(0, Math.min(normalizedRotation, 1));
+
+    const y = minY + normalizedRotation * (maxY - minY);
+
+    return [x, y, z];
+  }, [envRotation]);
+
+  console.log(
+    envRotation,
+    envRotation + Math.PI,
+    -0.1 + Math.PI,
+    -2.4 + Math.PI,
+    position
+  );
 
   return (
     <>
-      <ambientLight intensity={0.5} color={lighting} />
       <directionalLight
         castShadow
-        position={[-50, 10, 10]}
-        intensity={0.5}
+        position={position}
+        intensity={1.5}
         shadow-mapSize={4096}
         shadow-bias={0.2}
         color={lighting}
@@ -31,20 +63,34 @@ const Env = () => {
       </directionalLight>
       <fog
         attach="fog"
-        args={[blendColors(theme.background, "#9cafc0", intensity), 25, 50]}
+        args={[blendColors(theme.background, "#9cafc0", envIntensity), 15, 75]}
       />
       <Environment background>
         <color attach="background" args={[theme.background]} />
-        <Sphere scale={100} rotation={[0, y, 0]}>
+        <Sphere scale={100} rotation={[0, envRotation, 0]}>
           <meshBasicMaterial
             map={environmentTexture}
             side={BackSide}
             toneMapped={false}
             transparent
-            opacity={intensity}
+            opacity={envIntensity * 0.7}
           />
         </Sphere>
       </Environment>
+      <SoftShadows size={25} focus={0.01} samples={10} />
+      <ContactShadows
+        position={[0, groundY + 0.001, 0]}
+        frames={2}
+        ambient={0.2}
+        radius={10}
+        amount={4}
+        opacity={0.5}
+        scale={10}
+        blur={2}
+        far={10}
+        resolution={128}
+        color="#000"
+      />
     </>
   );
 };
